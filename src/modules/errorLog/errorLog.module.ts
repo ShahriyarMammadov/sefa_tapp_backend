c5247010a26ba26fcf5706105478244f5cfc4f02
shahriyarmammadov <shahriyarmammadov16@gmail.com>
2024-12-18 20:19:40 +0400

    send email service added in error-log

--- src/modules/errorLog/errorLog.module.ts
+++ src/modules/errorLog/errorLog.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ErrorLogController } from 'src/controller/errorLog/errorLog.controller';
 import { ErrorLog, ErrorLogSchema } from 'src/schema/errorLog';
+import { EmailService } from 'src/services/email/send-email.service';
 import { ErrorLogService } from 'src/services/errorLog/errorLog.service';
 
 @Module({
@@ -11,6 +12,6 @@ import { ErrorLogService } from 'src/services/errorLog/errorLog.service';
     ]),
   ],
   controllers: [ErrorLogController],
-  providers: [ErrorLogService],
+  providers: [ErrorLogService, EmailService],
 })
 export class ErrorLogModule {}
--- src/services/errorLog/errorLog.service.ts
+++ src/services/errorLog/errorLog.service.ts
@@ -3,39 +3,41 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateErrorLogDto } from 'src/dto/ErrorLog/errorLog.dto';
 import { ErrorLog } from 'src/schema/errorLog';
+import { EmailService } from '../email/send-email.service';
 
 @Injectable()
 export class ErrorLogService {
   constructor(
     @InjectModel(ErrorLog.name) private errorLogModel: Model<ErrorLog>,
+    private readonly emailService: EmailService,
   ) {}
 
-  async create(createErrorLogDto: CreateErrorLogDto): Promise<ErrorLog> {
+  async create(createErrorLogDto: CreateErrorLogDto): Promise<any> {
     const createdErrorLog = new this.errorLogModel(createErrorLogDto);
-    return createdErrorLog.save();
+    const savedErrorLog = await createdErrorLog.save();
+
+    this.sendEmailNotification(
+      'tu1d1f9a@code.edu.az',
+      'Tətbiqdə xəta baş verdi.',
+      `${createErrorLogDto?.request}`,
+    );
+
+    return savedErrorLog;
+  }
+
+  private async sendEmailNotification(
+    to: string,
+    subject: string,
+    content: string,
+  ): Promise<void> {
+    try {
+      await this.emailService.sendEmail(to, subject, content);
+    } catch (error) {
+      console.error('send email error:', error);
+    }
   }
 
   async getAll(): Promise<ErrorLog[]> {
     return this.errorLogModel.find().exec();
   }
-
-  // async filterByDate(startDate: string, endDate: string): Promise<ErrorLog[]> {
-  //   try {
-  //     const start = new Date(startDate);
-  //     const end = new Date(endDate);
-
-  //     if (isNaN(start.getTime()) || isNaN(end.getTime())) {
-  //       throw new Error('error time format');
-  //     }
-
-  //     return this.errorLogModel
-  //       .find({
-  //         createdAt: { $gte: start, $lte: end },
-  //       })
-  //       .exec();
-  //   } catch (error) {
-  //     console.error('Error filtering by date:', error);
-  //     throw new Error('Filter by date failed');
-  //   }
-  // }
 }
